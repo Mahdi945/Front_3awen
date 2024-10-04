e325b0b7319ce4048f3a4efd6b368567bedf19f0
Mahdi Bey <mahdibeyy@gmail.com>
2024-10-04 22:25:04 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    NavbarComponent,
+    HomeComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
